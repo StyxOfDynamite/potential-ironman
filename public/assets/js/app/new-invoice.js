e3dc171c0bca671c8407ce60252e850378e1bf59
Luke Storer <luke.storer@jadu.net>
2014-10-17 01:56:55 +0100

    [feature] added dynamic total generation for lines and invoice total

--- public/assets/js/app/new-invoice.js
+++ public/assets/js/app/new-invoice.js
@@ -1,8 +1,49 @@
 $(function() {
+	/** DatePicker functionality */
 	$('#due_date').datepicker({dateFormat: 'dd/mm/yy'});
 
+	/** calculate item total */
+	  $('input[name="item_qty[]"],input[name="item_price[]"]').on("change", function () {
+	  	var $container = $(this).closest('.form-group');
+	  	qty = Number($('input[name="item_qty[]"]',$container).val())||0,
+	  	price = Number($('input[name="item_price[]"]',$container).val())||0;
+
+	  	$('input[name="item_total[]"]',$container).val(qty * price);
+	  	$('input[name="item_total[]"]',$container).change();
+
+  });
+
+	/** Add additional item lines */
 	$('#add-item').click(function(){
 		$('.input-row:first').clone().insertAfter('.input-row:last');
-	})
+		$('input[name="item_qty[]"],input[name="item_price[]"]').on("change",function () {
+			var $container = $(this).closest('.form-group');
+	  		qty = Number($('input[name="item_qty[]"]',$container).val())||0,
+	  		price = Number($('input[name="item_price[]"]',$container).val())||0;
+	  		$('input[name="item_total[]"]',$container).val(qty * price);
+	  		$('input[name="item_total[]"]',$container).change();
+	  	});
+	  	/** Sum inputs for invoice total */
+		$('input[name="item_total[]"').change(function() {
+			var total = 0;
+			$.each($("[name='item_total[]']"), function(index, value) {
+				total += parseFloat($(this).val());
+			});
+			$("#total").val(total);
+		});
+		/** trigger change event so total is updated to inlcude newly added line */
+		$('input[name="item_total[]"').change();
+	});
+
+	
+
+	/** Sum inputs for invoice total */
+	$('input[name="item_total[]"').change(function() {
+		var total = 0;
+		$.each($("[name='item_total[]']"), function(index, value) {
+			total += parseFloat($(this).val());
+		});
+		$("#total").val(total);
+	});
 
  });
